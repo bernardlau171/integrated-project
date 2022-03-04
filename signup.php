74927f6debf508d818b48c44855d09403dbf2b64
Bernard Lau <21035849d@connect.polyu.hk>
2022-03-04 22:15:27 +0800

    added register, login, styles

--- signup.php
+++ signup.php
@@ -1,8 +1,34 @@
 <?php
 include_once 'header.php';
 ?>
-        <div class="wrapper">
-        <h1>signup</h1>
-        </div> 
+<section class="signupform">
+        <h2>Signup</h2>
+        <form action="signup.inc.php" method="post">
+            <div>
+                <label for="name">Name: </label>
+                <input type="text" name="name" placeholder="Full name">
+            </div>
+            <div>
+                <label for="email">E-mail: </label>
+                <input type="text" name="email" placeholder="E-mail">
+            </div>
+            <div>
+                <label for="uid">Username: </label>
+                <input type="text" name="uid" placeholder="username">
+            </div>
+            <div>
+                <label for="password">Password: </label>
+                <input type="password" name="pwd" placeholder="Password">
+            </div>
+            <div>
+                <label for="repeatpwd">Confirm password: </label>
+                <input type="password" name="repeatpwd" placeholder="re-enter password">
+            </div>
+            <button type="reset" name="reset">Reset</button>
+                
+            
+            <button type="submit" name="submit">Sign Up</button>
+        </form>
+</section>
     </body>
 </html>
