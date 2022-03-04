413c0a277fd9ec7178432bad2be3b6f708586665
Bernard Lau <21035849d@connect.polyu.hk>
2022-03-04 16:43:17 +0800

    first

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <title>Job hunting site</title>
+            <link rel="stylesheet" type="text/css" href="style.css">
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"> 
+        </head>
+        <body>
+        <div class="wrapper">
+        <nav id="navbar" class="navbar">
+                <ul class="navlist">
+                    <li class="navitem">
+                        <a href="#heading">GetaJob.com</a>
+                    </li>
+                    <li class="navitem">
+                        <a href="#heading">Home</a>
+                    </li>
+                    <li class="navitem">
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li class="navitem">
+                        <a href="signup.php">Sign up</a>
+                    </li>
+                    <li class="navitem">
+                        <button class="btn-primary">For Employers</button>
+                    </li>
+
+                </ul>
+            </nav>
+            </div>
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+include_once 'header.php';
+?>
+        <div class="wrapper">
+        <h1>login</h1>
+        </div>        
+    </body>
+</html>
