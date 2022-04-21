1d359260e1175c055338ea5f5120c02e6b7ebff3
Bernard Lau <21035849d@connect.polyu.hk>
2022-04-21 11:06:01 +0800

    bug fix

--- index-company.php
+++ index-company.php
@@ -13,7 +13,7 @@ include_once 'header.php';
             $query = mysqli_query($conn, $sql);
             $query2 = mysqli_query($conn, $sql2);
             echo "<h1>Welcome to company page, ".$uid."</h1><br>";
-            echo " <div class='text-center'><a href='create.php'>+ Create a new job post</a><br><br>Your listed jobs</div><div class='space'></div>";
+            echo " <div class='text-center'><a href='create.php'>+ Create a new job post</a></div><div class='space'></div>";
         }?>
         <section class="container">
         <h2>New applications</h2>
@@ -25,6 +25,7 @@ include_once 'header.php';
                 </div><?php } ?>
         </section>
         <scetion class="container">
+            <h2>Your listed jobs</h2>
             <?php foreach($query as $q){?>
                 <div class="card">
                     <h2><?php echo $q['jobTitle'] ?></h2>
