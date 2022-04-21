f8ab8ff1ca0c93bdaa7244ee887ced48f11a2dcd
Bernard Lau <21035849d@connect.polyu.hk>
2022-04-21 11:03:15 +0800

    application marginally ok

--- index-company.php
+++ index-company.php
@@ -9,10 +9,21 @@ include_once 'header.php';
         if(isset($_SESSION["useruid"])){
             $uid = $_SESSION["useruid"];
             $sql = "SELECT * FROM job where userUid = '$uid'";
+            $sql2 = "SELECT * FROM applicationmsg where Id in ('3','4','5')";
             $query = mysqli_query($conn, $sql);
+            $query2 = mysqli_query($conn, $sql2);
             echo "<h1>Welcome to company page, ".$uid."</h1><br>";
             echo " <div class='text-center'><a href='create.php'>+ Create a new job post</a><br><br>Your listed jobs</div><div class='space'></div>";
         }?>
+        <section class="container">
+        <h2>New applications</h2>
+        <?php foreach($query2 as $q2){?>
+                <div class="card">
+                    <h2><?php echo $q2['senderName'] ?></h2>
+                    <p><br>Salary: <?php echo $q2['jobId']?></p>
+                    <p><br><?php echo $q2['applymessage']?></p>
+                </div><?php } ?>
+        </section>
         <scetion class="container">
             <?php foreach($query as $q){?>
                 <div class="card">
